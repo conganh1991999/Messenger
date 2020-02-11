f2eed094286601c4b6ecbe7f4c60ac1d14ae4498
CongAnh <conganh1991999@gmail.com>
2020-02-11 21:22:27 +0700

    so lazy

--- app/src/main/java/com/camm/booking/NewMessageActivity.java
+++ app/src/main/java/com/camm/booking/NewMessageActivity.java
@@ -1,10 +1,21 @@
 package com.camm.booking;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class NewMessageActivity extends AppCompatActivity {
 
@@ -15,14 +26,64 @@ public class NewMessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_message);
 
-        Mapping();
-
         ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
         actionBar.setTitle("Select User");
+
+        Mapping();
+        getUsersFromDatabase();
+
     }
 
     private void Mapping(){
         recyclerviewNewMessage = findViewById(R.id.recyclerviewNewMessage);
+    }
+
+    private void fetchDataIntoRecycleView(ArrayList<User> listUser){
+        recyclerviewNewMessage.setHasFixedSize(true);
+
+        LinearLayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerviewNewMessage.setLayoutManager(manager);
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(listUser);
+        recyclerviewNewMessage.setAdapter(adapter);
+    }
+
+    private void getUsersFromDatabase(){
+        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference mRef = mDatabase.getReference("users");
+
+        mRef.addChildEventListener(new ChildEventListener() {
+
+            ArrayList<User> listUser = new ArrayList<>();
+
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                User user = dataSnapshot.getValue(User.class);
+                listUser.add(user);
+
+                fetchDataIntoRecycleView(listUser);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/camm/booking/RecyclerViewAdapter.java
+++ app/src/main/java/com/camm/booking/RecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.camm.booking;
 
-import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -8,45 +9,51 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
-    ArrayList<User> listUser;
-    Context context;
+    private ArrayList<User> listUser;
 
-    public RecyclerViewAdapter(ArrayList<User> listUser, Context context) {
+
+    RecyclerViewAdapter(ArrayList<User> listUser) {
         this.listUser = listUser;
-        this.context = context;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView txtUsername;
         CircleImageView imgUser;
 
-        public ViewHolder(@NonNull View itemView) {
+        ViewHolder(@NonNull View itemView) {
             super(itemView);
+            txtUsername = itemView.findViewById(R.id.rowUsername);
+            imgUser = itemView.findViewById(R.id.rowUserImage);
         }
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemView = inflater.inflate(R.layout.recycler_view_row, parent, false);
+        return new ViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.txtUsername.setText(listUser.get(position).getUserName());
+        Uri uri = Uri.parse(listUser.get(position).getUserImage());
+        Picasso.get().load(uri).into(holder.imgUser);
     }
 
     @Override
     public int getItemCount() {
         return listUser.size();
     }
-
 
 }
